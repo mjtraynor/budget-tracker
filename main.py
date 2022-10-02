f3a9de6942811645b1a9fa3e3a70ae35870ace04
Michael Traynor <traynormichaeljohn@gmail.com>
2022-10-02 11:57:21 -0500

    Added Python file basic structure

---
+++ main.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+def main():
+    """This runds the main code"""
+    ...
+
+
+def get_amex_statement():
+    """Connects to Amex website and downloads the latest
+    statement as an excel file
+    """
+    ...
+
+def get_chase_statement():
+    """Connects to Amex website and downloads the latest
+    statement as an excel file
+    """
+    ...
+
+def get_firsthorizon_statement():
+    """Connects to Amex website and downloads the latest
+    statement as an excel file
+    """
+    ...
+
+def move_statement():
+    """Move the statement from downlaods folder"""
+    ...
+
+def update_budget_file():
+    """Refresh the budget tracket Excel file"""
+    ...
+
+
+if __name__ == main():
+    main()
